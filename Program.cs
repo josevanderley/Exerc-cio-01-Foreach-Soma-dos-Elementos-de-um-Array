5e39e5f14da6ad5f7232b93648fb644e8c8d2421
josevanderley <89553942+josevanderley@users.noreply.github.com>
2023-12-03 15:48:38 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_01_Foreach__Soma_dos_Elementos_de_um_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            int soma = 0;
+
+            foreach (int elemento in array)
+            {
+                soma += elemento;
+            }
+
+            Console.WriteLine($"A soma dos elementos do array é: {soma}");
+
+            Console.ReadKey();  
+        }
+    }
+}
+    
